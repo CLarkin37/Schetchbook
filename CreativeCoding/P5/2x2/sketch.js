d957882ec848a5a978fa930a7b0a90193afdc4b1
CLarkin37 <cmlarkin37@icloud.com>
2021-11-23 09:50:02 -0500

    background

--- CreativeCoding/P5/2x2/sketch.js
+++ CreativeCoding/P5/2x2/sketch.js
@@ -1,21 +1,8 @@
 function setup() {
-  // runs ounces
-  createCanvas(720, 720);
-  fill("green");
-  strokeWeight(8);
+createCanvas (windowWidth, windowHeight);
+background (255,0,0);
 }
 
 function draw() {
-  // runs in a loop
-  background(220);
-  translate(60, 60);
-  quad(0, 0, 300, 0, 300, 300, 0, 300);
 
-  quad(300, 0, 600, 0, 600, 300, 300, 300); // increase x 300 px
-
-  quad(0, 300, 300, 300, 300, 600, 0, 600); // increase y 300 px
-
-  quad(300, 300, 600, 300, 600, 600, 300, 600); // increase y 300 px
-
-  noLoop();
 }
