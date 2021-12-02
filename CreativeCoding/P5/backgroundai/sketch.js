4a929f78e97a41c4bddd4fc490bf33e095a3e8c3
CLarkin37 <cmlarkin37@icloud.com>
2021-12-01 20:03:20 -0500

    20x20

---
+++ CreativeCoding/P5/backgroundai/sketch.js
@@ -0,0 +1,32 @@
+
+
+function preload () {
+  img = loadImage ('assets/background.png')
+}
+
+function setup() {
+  // runs ounces
+  createCanvas(720, 720);
+  fill("red");
+  strokeWeight(8);
+}
+
+function draw() {
+
+  background(220);
+  translate(60, 60);
+
+  for (var x = 0; x < 600; x = x + 300)
+
+  for (var y = 0; y < 600; y = y + 300)
+  
+  image (img, x, y)
+
+  // quad(x, y,
+  //      x + 300, y,
+  //      x + 300, y + 300,
+  //      x, y + 300);
+  //
+  //
+  // noLoop();
+}
