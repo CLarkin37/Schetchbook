0ae712490e48b1f6af8c8823a58c721900e9b902
CLarkin37 <cmlarkin37@icloud.com>
2021-12-01 20:10:47 -0500

    Update sketch.js

--- CreativeCoding/P5/backgroundai/sketch.js
+++ CreativeCoding/P5/backgroundai/sketch.js
@@ -1,7 +1,8 @@
 
+let img;
 
 function preload () {
-  img = loadImage ('assets/background.png')
+img = loadImage ('assets/background.png')
 }
 
 function setup() {
@@ -13,20 +14,20 @@ function setup() {
 
 function draw() {
 
-  background(220);
-  translate(60, 60);
+background(220);
 
-  for (var x = 0; x < 600; x = x + 300)
+translate (60,60);
 
-  for (var y = 0; y < 600; y = y + 300)
-  
-  image (img, x, y)
+  for (var x = 0; x < 600; x = x + 300) {
+    for (var y = 0; y < 600; y = y + 300) {
 
-  // quad(x, y,
+    image (img, 0, 0)
+  }
+  }  // quad(x, y,
   //      x + 300, y,
   //      x + 300, y + 300,
   //      x, y + 300);
   //
   //
-  // noLoop();
+  noLoop();
 }
