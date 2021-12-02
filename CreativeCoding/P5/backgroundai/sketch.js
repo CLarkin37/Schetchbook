c93aee1f2c9e20e96c3b03b138153992c52e7c93
CLarkin37 <cmlarkin37@icloud.com>
2021-12-01 20:59:56 -0500

    change

--- CreativeCoding/P5/backgroundai/sketch.js
+++ CreativeCoding/P5/backgroundai/sketch.js
@@ -2,7 +2,7 @@
 let img;
 
 function preload () {
-img = loadImage ('assets/background.png')
+img = loadImage ('assets/background.png');
 }
 
 function setup() {
@@ -14,16 +14,16 @@ function setup() {
 
 function draw() {
 
-background(220);
+  background(220);
 
-translate (60,60);
+// translate (60,60);
 
-  for (var x = 0; x < 600; x = x + 300) {
-    for (var y = 0; y < 600; y = y + 300) {
+  // for (var x = 0; x < 600; x = x + 300) {
+  //   for (var y = 0; y < 600; y = y + 300) {
 
-    image (img, 0, 0)
-  }
-  }  // quad(x, y,
+  image (img, 0, 0)
+
+    // quad(x, y,
   //      x + 300, y,
   //      x + 300, y + 300,
   //      x, y + 300);
