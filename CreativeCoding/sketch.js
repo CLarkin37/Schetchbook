05eb81aa13543bfcc200661cdc0996a16c5f67e3
CLarkin37 <cmlarkin37@icloud.com>
2021-12-07 08:17:36 -0500

    background

---
+++ CreativeCoding/sketch.js
@@ -0,0 +1,40 @@
+let gif;
+
+function preload(){
+  gif=loadImage('assets/background.gif');
+
+
+
+}
+
+
+function setup() {// runs ounces
+  createCanvas(windowWidth, windowHeight);
+  fill("red");
+  strokeWeight(1);
+}
+
+function draw() {
+
+var num = 9; // number of squares
+var sideLen = windowWidth/num; //side length of square
+
+  for (var x = 0; x < windowWidth; x = x + sideLen) {//squares in x direction
+    for (var y = 0; y < windowWidth; y = y + sideLen) {// in y direction
+image (gif,x,y);
+  }
+  function windowResized(){
+  resizeCanvas(windowWidth, windowHeight);
+}
+  }
+
+}
+
+  // quad(x, y,
+  //      x + 300, y,
+  //      x + 300, y + 300,
+  //      x, y + 300);
+
+
+
+  // noLoop();
