78cbcccecea2a536ea1459d13299235ca7043ce8
CLarkin37 <cmlarkin37@icloud.com>
2021-12-07 08:40:58 -0500

    background

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ var sideLen = windowWidth/num; //side length of square
 
   for (var x = 0; x < windowWidth; x = x + sideLen) {//squares in x direction
     for (var y = 0; y < windowWidth; y = y + sideLen) {// in y direction
-image(gif, x, y, windowWidth/num, windowWidth*.69/num);
+image(gif, x, y, windowWidth*.69/num, windowWidth*.69/num);
   }
   function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
