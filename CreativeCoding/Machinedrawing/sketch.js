f5a4f173c421b28a96476b4109b878888177fb1a
CLarkin37 <cmlarkin37@icloud.com>
2022-01-10 09:13:19 -0500

    machine drawing

---
+++ CreativeCoding/Machinedrawing/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+  x = random(int(0,400))
+  y = random(int(0,400))
+  z = random(int(0,400))
+
+function draw() {
+  background(220);
+  for i = 0; i < 50; i ++
+  triangle (10,10,40,20,30,30)
+}
